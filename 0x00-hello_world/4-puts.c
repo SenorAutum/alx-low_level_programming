1b7b55109a8634ab4dd15ed8e59f9174eeaf6705
SenorAutum <brandon.mwanzia@students.ac.ke>
2023-05-31 16:09:55 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /* Prints a line */
-int main(void)
+int main()
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
