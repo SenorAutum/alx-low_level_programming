6fa41e7dad8c1765d69992294ab1871831051d28
SenorAutum <brandon.mwanzia@students.ac.ke>
2023-06-02 08:17:06 +0000

    Genius to get simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char messo[] =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	putchar(messo);
+	return (0);
+}
