bd91b8363351de0cfbf50083c94ac43782d7a103
SenorAutum <brandon.mwanzia@students.ac.ke>
2023-06-02 08:27:39 +0000

    Genius to get simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	char messo[] =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	putchar(messo);
-	return (0);
+	const char messo[] =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, messo, sizeof(message) -1);
+	return (1);
 }
