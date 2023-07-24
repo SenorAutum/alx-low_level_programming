6dc2f5b30c9ce2f40f665f206f737c7f6c1916e5
SenorAutum <brandon.mwanzia@students.ac.ke>
2023-07-24 08:44:00 +0000

    Lower case check

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c : Character to be checked
  * return 1 if letter, lowercase or uppercase or return 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 }
