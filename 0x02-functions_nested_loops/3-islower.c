0a6c2aaad77043d8e05ee9eac89bdeb1def8bbc6
SenorAutum <brandon.mwanzia@students.ac.ke>
2023-07-24 08:16:59 +0000

    Lower case check

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main block
+ * _islower - checks for lower case
+ * Return (0) always success
+ */
+int _islower(int c)
+{
+	if (c <= '97'; c >= '122';)	
+	{
+		printf("c is lowercase");
+		return (1);
+	}
+
+	else
+	{
+		printf("c is uppercase");
+		return (0);
+	}
+}
+
