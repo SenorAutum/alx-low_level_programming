514c2a427e164b22d6743bd67f5321699a307299
SenorAutum <brandon.mwanzia@students.ac.ke>
2023-07-28 08:10:40 +0000

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * _putchar - print function
+ */
+void print_numbers(void)
+{
+	for (c >= '0' && c <= '9')
+	{	_putchar(c);
+	}
+	_putchar ('\n');
+}
