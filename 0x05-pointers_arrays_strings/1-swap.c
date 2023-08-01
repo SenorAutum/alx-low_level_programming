1222135a6e8dffd73688160803ef33190a11353d
SenorAutum <brandon.mwanzia@students.ac.ke>
2023-08-01 09:57:07 +0000

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - Swaps integers
+ * @ *a : to be swapped
+ * @ *b : to be swapped too
+ * return : void
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *b
+	*a = c;
+	*b = *a;
+}
