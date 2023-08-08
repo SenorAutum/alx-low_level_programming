aa45e502dd9285aac6b3c3369fc82e4d03c7c619
SenorAutum <brandon.mwanzia@students.ac.ke>
2023-08-08 16:36:43 +0000

    Updates

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int i);
 <<<<<<< HEAD
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
 =======
 int largest_number(int a, int b, int c);
 >>>>>>> ae54ef5a0e3f0d91b0d08f2cadfec43c31ede8b5
